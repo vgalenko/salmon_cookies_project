9a140f7494f41f5916a69cfeee6d551838d33579
vgalenko <vitaliygalenko@gmail.com>
2017-05-16 13:52:24 -0700

    updated my literal objects and converted them into constructors as well as functions into prototypes

--- app.js
+++ app.js
@@ -1,47 +1,27 @@
-var FirstPike = {
-  minimum: 23 ,
-  maximum: 65,
-  avgcookies: 6.3,
-  cookieSales: [],
+//method to calculate data for constructor objects of our 5 stores
+function Store(minimum, maximum, avgcookies) {
+  this.minimum = minimum;
+  this.maximum = maximum;
+  this.avgcookies = avgcookies;
+}
+//all the constructors for the stores and their data.
+var FirstPike = new Store(23,65,6.3);
+var SeaTacAirport = new Store(3,24,1.2);
+var SeattleCenter = new Store(11,38,3.7);
+var CapitolHill = new Store(20,38,2.3);
+var Alki = new Store(2,16,4.6);
 
-  randomNumGenerator: function() {
-    return Math.floor(Math.random() * (this.maximum - this.minimum)) + this.minimum;
-  },
-  randomCookieSales: function() {
-    for (var i = 0; i < 15; i++) {
-      this.cookieSales.push(Math.floor(this.avgcookies * this.randomNumGenerator()));
-    }
-    console.log(this.cookieSales);
-  }
+cookieSales: [];
+
+// this prototype generates the amount of random customers that come through the door
+Store.prototype.randomNumGenerator = function() {
+  return Math.floor(Math.random() * (this.maximum - this.minimum)) + this.minimum;
 };
-FirstPike.randomNumGenerator();
-
-
-
-// var SeaTacAirport = {
-//   minimum: 3,
-//   maximum: 24,
-//   avgcookies: 1.2,
-// };
-// SeaTacAirport;
-//
-// var SeattleCenter = {
-//   minimum: 11,
-//   maximum: 38,
-//   avgcookies: 3.7,
-// };
-// SeattleCenter;
-//
-// var CapitolHill = {
-//   minimum: 20,
-//   maximum: 38,
-//   avgcookies: 2.3,
-// };
-// CapitolHill;
-//
-// var Alki = {
-//   minimum: 2,
-//   maximum: 16,
-//   avgcookies: 4.6
-// };
-// Alki;
+//for loop is counting through the amount of cookies sold per hour within 15 hours.
+Store.prototype.randomCookieSales = function() {
+  for (var i = 0; i < 15; i++) {
+    this.cookieSales.push(Math.floor(this.avgcookies * this.randomNumGenerator()));
+  }
+  console.log(this.cookieSales);
+};
+Store.FirstPike.randomNumGenerator();
