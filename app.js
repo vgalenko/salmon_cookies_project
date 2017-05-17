41798683ebb67a71731ff2f3085dcef52a3c5a10
vgalenko <vitaliygalenko@gmail.com>
2017-05-16 23:19:11 -0700

    added links and nav bar to sales.html file

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-var storeHours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
-
 //method to calculate data for constructor objects of our 5 stores
 function Store(location, minimum, maximum, avgcookies) {
   this.location = location;
@@ -13,15 +11,17 @@ function Store(location, minimum, maximum, avgcookies) {
 Store.prototype.randomNumGenerator = function() {
   return Math.floor(Math.random() * (this.maximum - this.minimum)) + this.minimum;
 };
+// declared an array for store hours
+var storeHours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
 //for loop is counting through amount of walk in customers and cookies sold per-hour in 15 hour day.
 Store.prototype.randomCookieSales = function() {
-  for (var i = 0; i < storeHours.length; i++) {
+  for (var i = 0; i < 15; i++) {
     this.cookieSales.push(Math.floor(this.avgcookies * this.randomNumGenerator()));
   }
   console.log(this.cookieSales);
 };
 
-// displaying each array value of the cities in ul on to the browser
+// displaying each array value of the cities in ul to the browser
 function listHrs(store) {
   var container = document.createElement('div');
   container.innerHTML = '<p>' + store.location + '</p>';
@@ -29,7 +29,7 @@ function listHrs(store) {
 
   var list = document.createElement('ul');
   var list_arr = [];
-
+// looping through each storeHours array to display hours in browser
   for (var i = 0; i < storeHours.length; i++) {
     list_arr.push('<li>' + storeHours[i]+ ': ' + store.cookieSales[i] + '</li>');
 
@@ -46,7 +46,7 @@ var SeattleCenter = new Store('SeattleCenter',11,38,3.7);
 var CapitolHill = new Store('CapitolHill',20,38,2.3);
 var Alki = new Store('Alki',2,16,4.6);
 
-//storing results for each location of the amount of cookie sales
+//storing results for each location of the amount of cookie sales each hour
 FirstPike.randomCookieSales();
 SeaTacAirport.randomCookieSales();
 SeattleCenter.randomCookieSales();
